e4f9bcf1c3bd7d1b74f179105ad751a8b1d56058
Tyler Petresky <tnpetresky@gmail.com>
2016-09-16 11:46:58 -0400

    Added Bower support: run `bower install` to install Bower components

--- app.js
+++ app.js
@@ -104,6 +104,7 @@ app.use(function(req, res, next) {
   next();
 });
 app.use(express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }));
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
 
 /**
